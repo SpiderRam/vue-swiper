ae607ad30d6af09294dfd0239ecb516d10f79034
Carmen Owen <doctorsout@yahoo.com>
2019-05-23 21:52:55 -0400

    Update npm and add Router

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import BootstrapVue from "bootstrap-vue";
 import App from './App.vue';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);

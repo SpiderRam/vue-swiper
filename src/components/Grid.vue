17e11ae2930276a344975635d27f82fbbc8495bd
Carmen Owen <doctorsout@yahoo.com>
2020-06-14 11:57:15 -0400

    npm audit fix

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="container-fluid">
         <div class="row">
-            <app-header></app-header>  
+            <app-header></app-header>
         </div>
         <div class="row">
-            <app-swiper-cube></app-swiper-cube>    
+            <app-swiper-cube></app-swiper-cube>
         </div>
         <!-- <div class="row">
             Footer            
@@ -15,21 +15,21 @@
 <script>
 import SwiperCubeVue from './SwiperCube.vue';
 import HeaderVue from './Header.vue';
-import ScrollVue from './Scroll.vue';
-import AboutVue from './About.vue';
+// import ScrollVue from './Scroll.vue';
+// import AboutVue from './About.vue';
 
 export default {
     components: {
         appSwiperCube: SwiperCubeVue,
         appHeader: HeaderVue,
-        appScroll: ScrollVue,
-        appAbout: AboutVue
-    }
+        // appScroll: ScrollVue,
+        // appAbout: AboutVue
+    },
 };
 </script>
 
 <style scoped>
-    body {
+body {
     background-image: linear-gradient(#cfe2cf, #609f60);
-  }
+}
 </style>
